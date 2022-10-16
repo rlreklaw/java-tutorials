e5a0d85ad870362a58017eb90b6cdc5ad85010be
rwalker <robpub@robonco.com>
2022-10-16 12:25:35 -0500

    Test with nested Predicate classes
    Exercise #1.

--- generics/src/main/java/com/robonco/javatutorial/generics/ElementCounter.java
+++ generics/src/main/java/com/robonco/javatutorial/generics/ElementCounter.java
@@ -2,20 +2,48 @@ package com.robonco.javatutorial.generics;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.function.Predicate;
 
 public class ElementCounter {
-   public static void main(String[] args) {
+
+    static public class OddPredicate implements Predicate<Integer> {
+
+        @Override
+        public boolean test(Integer integer) {
+            return integer % 2 == 1;
+        }
+    }
+
+    static public class EvenPredicate implements Predicate<Integer> {
+
+        @Override
+        public boolean test(Integer integer) {
+            return integer % 2 == 0;
+        }
+    }
+
+
+    public static void main(String[] args) {
         Collection<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9);
 
-        // ===== using Lambda =====
+        // ===== test using Lambda =====
+        System.out.println("===== test with lambdas =====");
         int oddCount = countElements(list, e -> e % 2 == 1);
         System.out.printf("# Odd values: %d%n", oddCount);
 
         int evenCount = countElements(list, e -> e % 2 == 0);
         System.out.printf("# Even values: %d%n", evenCount);
 
+        // ===== test using nested classes =====
+        System.out.println("===== test with nested Predicate classes");
+        oddCount=999;
+        evenCount=999;
+
+        oddCount = countElements(list, new OddPredicate());
+        evenCount = countElements(list, new EvenPredicate());
+
+        System.out.printf("# Odd values: %d%n", oddCount);
+        System.out.printf("# Even values: %d%n", evenCount);
     }
 
 
